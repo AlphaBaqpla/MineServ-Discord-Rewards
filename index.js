6fc1518fcfe1a1fff2fd400b8a9856cf5709df3c
AlphaBaqpla <str14017355@gmail.com>
2023-01-21 18:42:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+//=========-DiscordRewards BY Alpha-=========
+//==================-VAR`S-==================
 const config = require('./config.json')
+const fs = require('fs')
 const server = require('http')
 const { parse } = require('querystring')
 var SHA256 = require("crypto-js/sha256")
-
+const { Client } = require('discord.js');
+const client = new Client({ intents: 32767 })
+const comms = require("./comms.js")
+client.config = config
+//==================-SERVER-==================
 server.createServer((req,res)=>{
     let buf = ''
     req.on("data",(c)=>{
@@ -29,6 +36,27 @@ function checkSign(project,username,timestamp,signature){
         return false
     }
 }
-
+//=================-DISCORD-==================
+const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
+for (const file of events){
+  const event = require(`./events/${file}`)
+  client.on(event.name, (...args) => event.execute(...args, client))
+}
+client.on('messageCreate', (msg) => {
+    if (msg.author.username != client.user.username && msg.author.discriminator != client.user.discriminator) {
+      var comm = msg.content.trim() + " "
+      var comm_name = comm.slice(0, comm.indexOf(" "));
+      var messArr = comm.split(" ");
+      for (comm_count in comms.comms) {
+        var comm2 = config.prefix + comms.comms[comm_count].name;
+        if (comm2 == comm_name) {
+          comms.comms[comm_count].out(client, msg, messArr);
+        }
+      }
+    }
+  }
+)
+client.login(config.token)
+//===================-MAIN-===================
 function main(proj,user,time,sign){
 }
